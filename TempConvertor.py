b5ae1af605676382eead21aedede2cf451cd6303
Shreejesh J Ballal <shreejeshballal@gmail.com>
2022-11-27 12:56:43 +0530

    Temp convertor

---
+++ TempConvertor.py
@@ -0,0 +1,6 @@
+#python script which prompts the user for a Celsius temperature, convert the temperature to Fahrenheit, and print out the converted temperature. (Note: Tf = Tc * 9/5 + 32)
+
+celsiusT = float(input("Enter Celsius temperature:"))
+
+fahrenheitT = celsiusT * 9/5 +32
+print("The fahrenheit temperature is ",fahrenheitT)
